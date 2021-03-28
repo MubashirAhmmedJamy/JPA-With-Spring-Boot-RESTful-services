428d5bd763593f957726fdb947b82408e94b31e2
Jamy <mubashirjamy@gmail.com>
2021-03-29 00:17:27 +0600

    Learning JPA with Spring Boot Restful Services

---
+++ src/main/java/spring_boot_jpa/JPA_RESTful_application.java
@@ -0,0 +1,21 @@
+package spring_boot_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class JPA_RESTful_application {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(JPA_RESTful_application.class, args);
+		
+		int i = 0;
+		
+		for(String beanName: context.getBeanDefinitionNames()) {
+			System.out.println("Bean: " + ++i +". "+beanName);
+		}
+		
+	}
+
+}
---
+++ src/test/java/spring_boot_jpa/JPA_RESTful_applicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JPA_RESTful_applicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
